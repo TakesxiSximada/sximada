255176b3ecb6fa3115f88ad7fcc938b02ce138c4
TakesxiSximada <takesxi.sximada@gmail.com>
2016-04-01 00:12:18 +0900

    refactoring

--- www/src/index.js
+++ www/src/index.js
@@ -1,18 +1,37 @@
 import m from 'mithril'
-import headers_views from 'msx-loader!./headers/views'
-import views from 'msx-loader!./views'
+// import headers_views from 'msx-loader!./headers/views'
+// import views from 'msx-loader!./views'
 
 
-m.route(document.body, '', {
-    '': {
-        view: (ctrl) => {
-            return m('div', [
-                views.sximada(ctrl),
-            ]);
-        },
-        controller: () => {
-            return {
-            }
-        },
+var header = {
+    controller: () => {
+        return {};
     },
-});
+    view: (ctrl) => {
+        return m('div', 'header');
+    },
+};
+var nav = {};
+var article = {};
+var footer = {};
+
+
+
+m.mount(document.querySelector('header'), header);
+// m.mount(document.querySelector('nav'), nav);
+// m.mount(document.querySelector('article'), article);
+// m.mount(document.querySelector('footer'), footer);
+
+// m.route(document.body, '', {
+//     '': {
+//         view: (ctrl) => {
+//             return m('div', [
+//                 views.sximada(ctrl),
+//             ]);
+//         },
+//         controller: () => {
+//             return {
+//             }
+//         },
+//     },
+// });
